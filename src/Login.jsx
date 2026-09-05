4e7e869dbe4a006f4c7c6c8524a88ac020ea311d
Inderjeet Singh <dragonarc977@gmail.com>
2026-09-05 14:29:49 +0530

    Fixed supabase import path

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import supabase from '../lib/supabase'; // Points to your default export
+import supabase from './lib/supabase'; // Points to your default export
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
